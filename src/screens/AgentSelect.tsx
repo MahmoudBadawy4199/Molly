dbdf5ad826e70abb831a8931ac1c735e69ae27ee
MahmoudBadawy4199 <m.badawy2011@gmail.com>
2022-10-30 16:32:03 +0200

    dependencies [navigation,splash] + initial config

---
+++ src/screens/AgentSelect.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const AgentSelect = () => {
+    return (
+        <View>
+            <Text>AgentSelect</Text>
+        </View>
+    );
+};
+
+export default AgentSelect;
+
+const styles = StyleSheet.create({});
