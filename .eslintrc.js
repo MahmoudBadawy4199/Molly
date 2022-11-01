855183f990b47ac29333ec3ff1bcfd26d77405b4
MahmoudBadawy4199 <m.badawy2011@gmail.com>
2022-11-01 09:00:56 +0200

    eslint error fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     rules: {
         'react/jsx-uses-react': 'off',
         'react/react-in-jsx-scope': 'off',
+        'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
 };
