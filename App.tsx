37f98699e6f38a187670577d705fe25dfeef8d93
MahmoudBadawy4199 <m.badawy2011@gmail.com>
2022-11-08 00:22:57 +0200

    disable RTL

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import RootNavigator from './src/navigation/RootNavigator';
+import { I18nManager } from 'react-native';
+
+// Disable RTL
+I18nManager.forceRTL(false);
+I18nManager.allowRTL(false);
 
 export default function App() {
     const [loaded] = useFonts({
