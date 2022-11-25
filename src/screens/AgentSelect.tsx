b7d7b0bf01cf0dd40337e9eef8e6544b073b0c2d
MahmoudBadawy4199 <m.badawy2011@gmail.com>
2022-11-25 21:35:07 +0200

    agent select screen [ new ui ] + mock json data

--- src/screens/AgentSelect.tsx
+++ src/screens/AgentSelect.tsx
@@ -1,64 +1,67 @@
 // React
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import {
+    StyleSheet,
+    View,
+    FlatList,
+    ListRenderItem,
+    ListRenderItemInfo,
+    TouchableOpacity,
+} from 'react-native';
 // Libraries
 import { useNavigation } from '@react-navigation/native';
 // Utils
 import Colors from '../utils/Colors';
-import { moderateScale, verticalScale } from '../utils/Scale';
+import { horizontalScale, moderateScale, verticalScale } from '../utils/Scale';
 // Components
 import Container from '../components/container-with-background-overlay';
+import Banner from '../components/banner';
 import BackgroundGradient from '../components/background-gradient';
 import Agent from '../components/agent';
 // Types
-import { AgentScreenNavigationProp } from '../types';
-// Assets
-import images from '../assets/images';
+import { AgentType, AgentScreenNavigationProp } from '../types';
+// Data
+import data from '../../data.json';
 
 const AgentSelect = () => {
+    // Navigation
     const navigation = useNavigation<AgentScreenNavigationProp>();
-    function navigationHandler() {
-        navigation.navigate('MapSelect');
+    function navigationHandler(agentID: number) {
+        navigation.navigate('MapSelect', { agentID });
     }
+
+    // Data
+    const { agents } = data;
+
+    // Agent Item
+    const renderAgentItem: ListRenderItem<AgentType> = ({
+        item,
+    }: ListRenderItemInfo<AgentType>) => (
+        <TouchableOpacity activeOpacity={1} onPress={() => navigationHandler(item.id)}>
+            <Agent data={item} />
+        </TouchableOpacity>
+    );
     return (
         <View style={styles.main}>
-            {/* Gradient */}
-            <BackgroundGradient style={styles.backgroundGradientStyle} />
             {/* Container with Background overlay */}
             <Container style={styles.containerStyle}>
-                {/* Label */}
-                <Text style={styles.labelStyle}>select your agent</Text>
-                {/* Static Agent Item*/}
-                <View style={styles.agentContainerStyle}>
-                    <Agent
-                        gradientColors={[
-                            'rgba(104, 20, 0, 1)',
-                            'rgba(159, 58, 4,1)',
-                            'rgba(183, 151, 129,1)',
-                            'rgba(255, 237, 203,1)',
-                            'rgba(255, 237, 173,1)',
-                        ]}
-                        shadowColor="#FF972F"
-                        modelImage={images.brim}
-                        nameImage={images.brimName}
-                        roleImage={images.role}
-                        onPress={navigationHandler}
-                    />
-                    <Agent
-                        gradientColors={[
-                            'rgba(33, 55, 38, 1)',
-                            'rgba(58, 115, 97,1)',
-                            'rgba(100, 161, 113,1)',
-                            'rgba(176, 255, 176,1)',
-                            'rgba(173, 255, 200,1)',
-                        ]}
-                        shadowColor="#89F476"
-                        modelImage={images.viper}
-                        nameImage={images.viperName}
-                        roleImage={images.role}
-                        onPress={navigationHandler}
-                    />
-                </View>
+                {/* Banner */}
+                <Banner
+                    screenTitle="choose your agent"
+                    defaultBackgroundImage={'Select Agent'}
+                    backgroundImageBlurRadius={3}
+                />
+                {/* Colorful Background Gradient */}
+                <BackgroundGradient style={styles.backgroundGradientStyle} />
+                {/* Agents */}
+                <FlatList
+                    style={styles.flatListStyle}
+                    data={agents}
+                    keyExtractor={(item) => item.id.toString()}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={styles.flatListContentContainerStyle}
+                    renderItem={renderAgentItem}
+                />
             </Container>
         </View>
     );
@@ -73,25 +76,30 @@ const styles = StyleSheet.create({
     },
     containerStyle: {
         flex: 1,
-        paddingHorizontal: moderateScale(16),
     },
     backgroundGradientStyle: {
         width: '100%',
         height: '100%',
         position: 'absolute',
+        top: verticalScale(140),
+        zIndex: -2,
     },
     labelStyle: {
+        ...StyleSheet.absoluteFillObject,
         color: Colors.white,
         fontSize: moderateScale(38),
         fontFamily: 'Tungsten',
         textTransform: 'uppercase',
-        marginTop: verticalScale(32),
+        alignSelf: 'center',
         textShadowOffset: { width: 0, height: verticalScale(4) },
         textShadowRadius: moderateScale(2),
         textShadowColor: Colors.black,
     },
-    agentContainerStyle: {
-        flex: 1,
-        justifyContent: 'space-evenly',
+    flatListStyle: {
+        marginTop: verticalScale(5),
+    },
+    flatListContentContainerStyle: {
+        paddingVertical: verticalScale(20),
+        paddingHorizontal: horizontalScale(16),
     },
 });
