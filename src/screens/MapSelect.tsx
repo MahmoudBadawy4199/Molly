6fc634569a34238ded820f46bbd3543d1f542d7d
MahmoudBadawy4199 <m.badawy2011@gmail.com>
2022-11-26 03:43:24 +0200

    map select screen refactor [ better structure ]

--- src/screens/MapSelect.tsx
+++ src/screens/MapSelect.tsx
@@ -1,8 +1,8 @@
 // React
 import React from 'react';
-import { StyleSheet, FlatList, View, ListRenderItem, ListRenderItemInfo } from 'react-native';
+import { StyleSheet, FlatList, ListRenderItem, ListRenderItemInfo } from 'react-native';
 // Libraries
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute, RouteProp } from '@react-navigation/native';
 // Components
 import Container from '../components/container-with-background-overlay';
 import Banner from '../components/banner';
@@ -11,143 +11,83 @@ import MapItem from '../components/map-item';
 // Utils
 import Colors from '../utils/Colors';
 import { horizontalScale, verticalScale } from '../utils/Scale';
-// Assets
-import images from '../../src/assets/images';
 // Types
-import { MapSelectScreenNavigationProp } from '../types';
+import { HomeStackParamList, MapItemType, MapSelectScreenNavigationProp } from '../types';
 
-// Static Mock Data for UI
-type mockType = {
-    id?: number;
-    title: string;
-    image?: number;
-};
+import data from '../../data.json';
+
 const MapSelect = () => {
-    // Navigation
+    // Navigation & Params
+    const route = useRoute<RouteProp<HomeStackParamList, 'MapSelect'>>();
+    const { agentID } = route.params;
     const navigation = useNavigation<MapSelectScreenNavigationProp>();
-    function navigationHandler() {
-        navigation.navigate('LineupSelect');
+    function navigationHandler(mapItem: MapItemType) {
+        navigation.navigate('LineupSelect', { agentID, mapItem });
     }
+    // Data Manipulation
+    const modelImage = data.agents[agentID].modelImage;
+    const allMapsThatAgentHasLineups = data.agents[agentID].lineups.map((item) => {
+        return data.maps[item];
+    });
 
-    // Static Mock Data
-    const mock: mockType[] = [
-        {
-            id: 0,
-            title: 'ascent',
-            image: images.ascent,
-        },
-        {
-            id: 1,
-            title: 'fracture',
-            image: images.ascent,
-        },
-        {
-            id: 2,
-            title: 'bind',
-            image: images.ascent,
-        },
-        {
-            id: 3,
-            title: 'breeze',
-            image: images.ascent,
-        },
-        {
-            id: 4,
-            title: 'icebox',
-            image: images.ascent,
-        },
-        {
-            id: 5,
-            title: 'haven',
-            image: images.ascent,
-        },
-        {
-            id: 6,
-            title: 'pearl',
-            image: images.ascent,
-        },
-        {
-            id: 7,
-            title: 'split',
-            image: images.ascent,
-        },
-    ];
-
-    // Flat List Item Render
-    const renderMapItem: ListRenderItem<mockType> = ({ item }: ListRenderItemInfo<mockType>) => (
+    // Render Map items
+    const renderMapItem: ListRenderItem<MapItemType> = ({
+        item,
+    }: ListRenderItemInfo<MapItemType>) => (
         <MapItem
             style={styles.mapItemStyle}
-            mapName={item.title}
-            mapImage={item.image}
-            onPress={navigationHandler}
+            mapData={item}
+            onPress={() => navigationHandler(item)}
         />
     );
+
     return (
-        <View style={styles.main}>
-            {/* Container with Background overlay */}
-            <Container style={styles.containerStyle}>
+        <>
+            <Container>
                 {/* Banner */}
-                <View style={styles.bannerStyle}>
-                    <Banner
-                        screenTitle="choose map"
-                        backgroundImage={images.mapSelectBackground}
-                        agentImage={images.viperMap}
-                    />
-                </View>
-                {/* Body  */}
-                <View style={styles.bodyStyle}>
-                    {/* Colorful Background Gradient */}
-                    <BackgroundGradient style={styles.backgroundGradientStyle} />
-                    {/* Map List */}
-                    <FlatList
-                        data={mock}
-                        numColumns={2}
-                        columnWrapperStyle={styles.flatListColumnStyle}
-                        renderItem={renderMapItem}
-                    />
-                </View>
+                <Banner
+                    screenTitle="select map"
+                    defaultBackgroundImage="Select Map"
+                    modelImage={modelImage}
+                />
+                {/* Background Gradient */}
+                <BackgroundGradient style={styles.backgroundGradientStyle} />
+
+                {/* Map List */}
+                <FlatList
+                    style={styles.flatListStyle}
+                    data={allMapsThatAgentHasLineups}
+                    numColumns={2}
+                    columnWrapperStyle={styles.flatListColumnStyle}
+                    renderItem={renderMapItem}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={styles.flatListContentContainerStyle}
+                />
             </Container>
-        </View>
+        </>
     );
 };
 
 export default MapSelect;
 
 const styles = StyleSheet.create({
-    main: {
-        backgroundColor: Colors.darknavy,
-        flex: 1,
-    },
-    containerStyle: {
-        flex: 1,
-    },
-    bannerStyle: {
-        flex: 0.5,
-        shadowColor: Colors.black,
-        shadowOffset: {
-            width: 0,
-            height: verticalScale(2),
-        },
-        shadowOpacity: 0.5,
-        shadowRadius: 5,
-        elevation: 12,
-        zIndex: 10,
-        borderBottomWidth: 0.4,
-        borderColor: Colors.black,
-    },
-    bodyStyle: {
-        flex: 1,
-    },
     backgroundGradientStyle: {
         width: '100%',
         height: '100%',
-        zIndex: -2,
         position: 'absolute',
+        top: verticalScale(140),
+        zIndex: -2,
+    },
+    flatListStyle: {
+        marginTop: verticalScale(2),
     },
     flatListColumnStyle: {
         justifyContent: 'space-between',
         paddingVertical: verticalScale(10),
-        paddingHorizontal: horizontalScale(12),
+    },
+    flatListContentContainerStyle: {
+        paddingVertical: verticalScale(12),
+        paddingHorizontal: horizontalScale(16),
     },
     mapItemStyle: {
         width: '48%',
