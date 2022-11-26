9e0953c4feb44bcb60eda8966a61a47a8ebd4331
MahmoudBadawy4199 <m.badawy2011@gmail.com>
2022-11-26 03:38:34 +0200

    container refactor [ better structure + naming ]

--- src/components/container-with-background-overlay.tsx
+++ src/components/container-with-background-overlay.tsx
@@ -1,23 +1,38 @@
-import { StyleSheet, View, ImageBackground, StyleProp, ViewStyle } from 'react-native';
+// React
 import React from 'react';
+import { StyleSheet, View, ImageBackground } from 'react-native';
+// Assets
 import images from '../assets/images';
+// Utils
+import Colors from '../utils/Colors';
 
 type ContainerProps = {
     children?: React.ReactNode;
-    style?: StyleProp<ViewStyle> | undefined;
 };
-
-const Container: React.FC<ContainerProps> = ({ children, style }) => {
+/**
+ * Definition:
+ * a Parent View With Default Background Color and an Overlay for all screens.
+ */
+const Container: React.FC<ContainerProps> = ({ children }) => {
     return (
-        <ImageBackground source={images.backgroundOverlay} style={styles.backgroundOverlay}>
-            <View style={style}>{children}</View>
-        </ImageBackground>
+        <View style={styles.containerStyle}>
+            <ImageBackground source={images.backgroundOverlay} style={styles.backgroundOverlay}>
+                <View style={styles.childrenContainerStyle}>{children}</View>
+            </ImageBackground>
+        </View>
     );
 };
 
 export default Container;
 
 const styles = StyleSheet.create({
+    containerStyle: {
+        flex: 1,
+        backgroundColor: Colors.darknavy,
+    },
+    childrenContainerStyle: {
+        flex: 1,
+    },
     backgroundOverlay: {
         width: '100%',
         height: '100%',
