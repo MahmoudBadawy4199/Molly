3ac42df463464a76ac1201fbb77c4a2873b9d25b
MahmoudBadawy4199 <m.badawy2011@gmail.com>
2022-12-05 21:32:21 +0200

    Refactor Assists [ Remove Mocking Assets ]

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -5,21 +5,6 @@ const images = {
     agentSelectBannerImage: require('./app/select-agents-background-image.webp'),
     favouritesBannerImage: require('./app/favourites.webp'),
     emptyFavouritesImage: require('./app/empty.webp'),
-    // mock
-    brim: require('./mock/brimstone-img2.png'),
-    viper: require('./mock/viper-img.png'),
-    viperName: require('./mock/viper-title-img.png'),
-    brimName: require('./mock/brim-title-img.png'),
-    role: require('./mock/role-icon.png'),
-    brimMap: require('./mock/brim-map.png'),
-    viperMap: require('./mock/viper-map.png'),
-    ascent: require('./mock/ascent.webp'),
-    minimap: require('./mock/minimap.png'),
-    lineupMap: require('./mock/lineup-map.png'),
-    lineup: require('./mock/lineup.png'),
-    position: require('./mock/position.png'),
-    result: require('./mock/result.png'),
-    spike: require('./mock/spike.png'),
 };
 
 export default images;
