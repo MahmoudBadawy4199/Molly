6251425cfb6f0680870e46012d58dc064283e6c8
MahmoudBadawy4199 <m.badawy2011@gmail.com>
2022-12-13 04:50:16 +0200

    press Back button behaviour

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -24,6 +24,7 @@ const TabNavigator = () => {
     return (
         <BottomTabNavigator.Navigator
             initialRouteName="Home"
+            backBehavior="none"
             screenOptions={({ route }) => ({
                 headerShown: false,
                 // Tab Bar Styling
